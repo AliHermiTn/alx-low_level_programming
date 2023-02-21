ee96330dad42dda8a37a52463d2f5c7262b5c3c6
AliHermiTN <alihermi25@gmail.com>
2023-02-21 13:50:36 -0800

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *main - print _putchar
+ *Return: 0
+ */
+int main(void)
+{
+_putchar('_');
+_putchar('P');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
